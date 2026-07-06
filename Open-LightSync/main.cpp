6b92e9b982815be98769051ec0df62b35f7d7e67
Kevin <169435465+kevin909867@users.noreply.github.com>
2026-07-06 20:37:52 +1000

    Revert "fsdfgdzfg"
    This reverts commit a66fcd72b61cd778aab1f254c59cf122205587a7.

--- Open-LightSync/main.cpp
+++ Open-LightSync/main.cpp
@@ -6,13 +6,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setQuitOnLastWindowClosed(false);
     a.setApplicationName("Open-LightSync");
     a.setApplicationDisplayName("Open-LightSync");
     a.setWindowIcon(QIcon("icon.ico"));
     MainWindow w;
-    w.showNormal();
-    w.raise();
-    w.activateWindow();
+    w.show();
     return QApplication::exec();
 }
