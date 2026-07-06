a66fcd72b61cd778aab1f254c59cf122205587a7
Kevin <169435465+kevin909867@users.noreply.github.com>
2026-07-06 20:32:18 +1000

    fsdfgdzfg

--- Open-LightSync/main.cpp
+++ Open-LightSync/main.cpp
@@ -6,10 +6,13 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setQuitOnLastWindowClosed(false);
     a.setApplicationName("Open-LightSync");
     a.setApplicationDisplayName("Open-LightSync");
     a.setWindowIcon(QIcon("icon.ico"));
     MainWindow w;
-    w.show();
+    w.showNormal();
+    w.raise();
+    w.activateWindow();
     return QApplication::exec();
 }
